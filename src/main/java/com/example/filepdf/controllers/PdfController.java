59c2f2f9873b446a6ec70d96cfddd6eadeae9b97
Thien Duy <nguyenvthienduy@gmail.com>
2023-10-02 16:43:22 +0700

    add controller home

--- src/main/java/com/example/filepdf/controllers/PdfController.java
+++ src/main/java/com/example/filepdf/controllers/PdfController.java
@@ -3,6 +3,7 @@ package com.example.filepdf.controllers;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,11 @@ import com.example.filepdf.services.IPdfService;
 public class PdfController {
 	@Autowired
 	private IPdfService iPdfService;
+
+	@GetMapping("/")
+	public String home() {
+		return "Export file pdf";
+	}
 
 	@PostMapping("/generate-pdf")
 	public String generateFilePdf(@RequestBody Map<String, Object> params) {
